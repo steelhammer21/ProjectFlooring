12fa180b0d82feabd7cb3bfd138f63c6f1ace4fd
steelhammer21 <fred@elcomcomponents.com>
2023-04-25 15:32:21 -0700

    Declaring the variable before conditions

--- Program.cs
+++ Program.cs
@@ -30,9 +30,7 @@ namespace ProjectFlooring
                 Console.WriteLine($"You need {tilesNeeded} tiles to cover the floor");
                 Console.WriteLine($"The total cost of tiles is ${totalCost}");
             }
-
             else
-
             {
                 Console.Write("Enter the base of the floor in foot: ");
                 double width = Convert.ToDouble(Console.ReadLine());
