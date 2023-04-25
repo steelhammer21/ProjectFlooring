1ace446af7587e4a52c7868bf1241ba4ca4ebafb
steelhammer21 <fred@elcomcomponents.com>
2023-04-25 15:34:53 -0700

    Declaring the variable before conditions (revised)

--- Program.cs
+++ Program.cs
@@ -17,20 +17,10 @@ namespace ProjectFlooring
                 Console.Write("Enter the length of the floor in foot: ");
                 double length = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Enter the area of a single tile in square foot: ");
-                double tileArea = Convert.ToDouble(Console.ReadLine());
-
-                Console.Write("Enter the cost per tile in dollars: ");
-                double tileCost = Convert.ToDouble(Console.ReadLine());
-
                 double floorArea = width * length;
-                double tilesNeeded = floorArea / tileArea;
-                double totalCost = tilesNeeded * tileCost;
-
-                Console.WriteLine($"You need {tilesNeeded} tiles to cover the floor");
-                Console.WriteLine($"The total cost of tiles is ${totalCost}");
+                Console.WriteLine(floorArea);
             }
-            else
+            else if (roomShape == "T")
             {
                 Console.Write("Enter the base of the floor in foot: ");
                 double width = Convert.ToDouble(Console.ReadLine());
@@ -38,13 +28,17 @@ namespace ProjectFlooring
                 Console.Write("Enter the height of the floor in foot: ");
                 double height = Convert.ToDouble(Console.ReadLine());
 
+                double floorArea = 0.5 * width * height;
+                Console.WriteLine(floorArea);
+            }
+
                 Console.Write("Enter the area of a single tile in square foot: ");
                 double tileArea = Convert.ToDouble(Console.ReadLine());
 
                 Console.Write("Enter the cost per tile in dollars: ");
                 double tileCost = Convert.ToDouble(Console.ReadLine());
 
-                double floorArea = 0.5 * width * height;
+                
                 double tilesNeeded = floorArea / tileArea;
                 double totalCost = tilesNeeded * tileCost;
 
@@ -52,7 +46,6 @@ namespace ProjectFlooring
                 Console.WriteLine($"The total cost of tiles is ${totalCost}");
 
 
-            }
         }
     }
 }
