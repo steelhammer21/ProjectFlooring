18367b140ce3cfde4f5ef6ff31e2057fedb54e3a
steelhammer21 <fred@elcomcomponents.com>
2023-05-10 16:31:48 -0700

    Moved all the shape specific code inside the conditions.

--- Program.cs
+++ Program.cs
@@ -6,26 +6,34 @@ namespace ProjectFlooring
     {
         static void Main(string[] args)
         {
-            
+
             Console.Write("Is the room rectangle or triangle (R/T) :");
             string roomShape = Console.ReadLine();
 
-            Console.Write("Enter the width of the floor in foot: ");
-            double width = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Enter the length of the floor in foot: ");
-            double length = Convert.ToDouble(Console.ReadLine());
+            double width = 0;
+            double length = 0;
             double floorArea = 0;
 
             if (roomShape == "R")
             {
+                Console.Write("Enter the width of the floor in foot: ");
+                width = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the length of the floor in foot: ");
+                length = Convert.ToDouble(Console.ReadLine());
+
                 floorArea = width * length;
-                Console.WriteLine($"The room size is {floorArea} square feet");
+
             }
             else if (roomShape == "T")
             {
+                Console.Write("Enter the height of the floor in foot: ");
+                width = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the length of the floor in foot: ");
+                length = Convert.ToDouble(Console.ReadLine());
+
                 floorArea = 0.5 * width * length;
-                Console.WriteLine($"The room size is {floorArea} square feet");
             }
             else
             {
@@ -41,6 +49,7 @@ namespace ProjectFlooring
             double tilesNeeded = floorArea / tileArea;
             double totalCost = tilesNeeded * tileCost;
 
+            Console.WriteLine($"The room size is {floorArea} square feet");
             Console.WriteLine($"You need {tilesNeeded} tiles to cover the floor");
             Console.WriteLine($"The total cost of tiles is ${totalCost}");
 
